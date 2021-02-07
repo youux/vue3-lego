856fe700e5a48c79fb66078383516d3d518769ec
尤金金 <jinjin.you@baozun.com>
2021-02-07 13:33:48 +0800

    0207-01

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
+// 引入模板
 import App from './App.vue'
-import './registerServiceWorker'
+// 引入路由
 import router from './router'
+// 引入vuex
 import store from './store'
+// 引入组件库
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+// 重置全局样式
+import './assets/css/reset.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus)
+  .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-aside></el-aside>
+    <el-main>
+      <el-header></el-header>
+      <div class="page-content" :style="{height:contentHei+'px'}">
+        <router-view />
+      </div>
+    </el-main>
+    <el-aside></el-aside>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { onMounted, reactive, toRefs } from 'vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup () {
+    onMounted(() => {
+      // 默认执行一次
+      getHeight()
+      // 改变窗口大小时执行
+      window.onresize = () => getHeight()
+    })
+    // 定义默认值
+    const data = reactive({
+      contentHei: 0
+    })
+    // 获取高度
+    const getHeight = () => {
+      data.contentHei = window.innerHeight - 120
+    }
+
+    // 数据返回给模板
+    return {
+      ...toRefs(data)
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.el-container {
+  width: 100%;
+  height: 100%;
+  background-color: #e9eef3;
+}
+.el-aside {
+  background-color: #fff;
+}
+.el-header {
+  background-color: #fff;
+}
+.page-content {
+  margin-top: 20px;
+  background-color: #fff;
+}
+
+</style>
